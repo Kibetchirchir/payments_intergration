6c0a1fa94174721a7a98d86fa68a80befae3aaa3
chirchir <langatchirchir@gmail.com>
2020-09-22 14:22:14 +0200

    featAuth): user should be able to signUp
    - add db connection
    - add route
    - add controllet for sign up
    
    [Finishes]

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response} from 'express';
+import api from './resources/api';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api', api);
 
 app.get('/', (req: Request, res: Response): Response => {
     return res.status(200).send({message: "hello world"})
